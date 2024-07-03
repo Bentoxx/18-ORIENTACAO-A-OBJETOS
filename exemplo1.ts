56c803286b12e307c92db0ea695f2729177294bc
Bento Netto <bentoeemanuele@gmail.com>
2024-07-03 17:31:14 -0300

    evolução do construtor

--- exemplo1.ts
+++ exemplo1.ts
@@ -1,23 +1,36 @@
+type TCarro = {
+    cor: string
+    marca: string
+    modelo: string
+    ano: number
+    potencia?: number
+}
 class Carro {
     cor: string 
     marca: string 
     modelo: string 
     ano: number
     potencia: number
-    constructor() {
-        this.cor = ''
-        this.marca = ''
-        this.modelo = ''
-        this.ano = 0
-        this.potencia = 0
+    constructor( carro: TCarro) {
+        this.cor = carro.cor
+        this.marca = carro.marca
+        this.modelo = carro.modelo
+        this.ano = carro.ano
+        this.potencia = carro.potencia ?? 90
     }
     
 }
-const fusca = new Carro()
-fusca.ano = 1970
-fusca.cor = 'Preto'
-fusca.marca = 'Volkswagem'
-fusca.modelo = 'Fusca'
-fusca.potencia = 90
+const fusca = new Carro({
+    cor: 'Preto',
+    marca: 'Volkswagem',
+    modelo: 'Fusca',
+    ano: 1970,
+    potencia: 120
+})
+// fusca.ano = 1970
+// fusca.cor = 'Preto'
+// fusca.marca = 'Volkswagem'
+// fusca.modelo = 'Fusca'
+// fusca.potencia = 90
 
 console.log(fusca);
